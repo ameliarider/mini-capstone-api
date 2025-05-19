ac1892bba34f54c58dd206d8276b7f035b5cd823
ameliarider <mimirider89@gmail.com>
2025-05-19 16:59:12 -0700

    jbuilder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products
+    @products = Product.all
+    render :index
   end
 
   def show
-    product = Product.find(params["id"])
-    render json: product
+    @product = Product.find(params["id"])
+    render :show
   end
 
   def create
