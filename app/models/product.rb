1cd267d25786a6f8473b5e110f040902a40b09cf
ameliarider <mimirider89@gmail.com>
2025-05-19 17:47:19 -0700

    New methods for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+  def formatted_price
+    "$#{price}"
+  end
+
+  def formatted_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def is_discounted?
+    if price < 10
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+    "$#{tax}"
+  end
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,5 +1,8 @@
 json.id product.id
 json.name product.name
-json.price product.price
+json.price product.formatted_price
 json.image_url product.image_url
 json.description product.description
+json.product_launched product.formatted_created_at
+json.product_discounted product.is_discounted?
+json.tax product.tax
